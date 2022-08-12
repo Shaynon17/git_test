6df1a1e5a0fc289319803813129317b014ae607d
Shannon Miller <shannon.wlmiller@yahoo.com>
2022-08-12 12:07:11 -0600

    template complete

--- ninjaGame/game.js
+++ ninjaGame/game.js
@@ -23,41 +23,12 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    // this.load.setBaseURL('http://localhost:80/ninja'); 
-    //Here we want to preload ALL assesets the game needs such as images and art, images, sprite
-    // game.load.image('sky', 'assets/sky.png')
-    // this.load.image('sky', sky);
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('sky', 'assets/sky.png')
 }
-
-// function create() {
-//     // game.physics.startSystem(Phaser.Physics.ARCADE)
-//     game.add.image(400, 300, 'sky')
-//     // game.add.sprite(0, 0, 'sky')
-// }
 
 function create() {
-    this.add.image(400, 300, 'sky');
-
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+    this.add.image(400, 300, 'sky')
 }
+
 
 function update() { }
