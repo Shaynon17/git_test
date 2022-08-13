98395cc4d0bef226d5324899deacc7aff48670d4
Shannon Miller <shannon.wlmiller@yahoo.com>
2022-08-13 08:06:37 -0600

    platforms and character added

--- ninjaGame/game.js
+++ ninjaGame/game.js
@@ -13,39 +13,91 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 300 },
+            debug: false
         }
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('sky', 'assets/sky.png')
-    this.load.image('ground', 'assets/ground.png')
+    this.load.image('sky', 'assets/newSky.png')
+    this.load.image('ground', 'assets/platform.png')
+    this.load.image('dude', 'assets/ninja.png')
+    // this.load.image('myGround', 'assets/myplatform.png')
 }
 
 function create() {
     // game.physics.startSystem(Phaser.Physics.ARCADE) //this line of code breaks the game
     this.add.image(400, 300, 'sky')
-    // this.add.image(200, 200, 'ground')
-    platforms = this.add.group()
-    platforms.enableBody = true
 
-    let ground = platforms.create(0, this.world.height - 64, 'ground')
-    ground.scale.setTo(2, 2)
-    ground.body.immovable = true
+    platforms = this.physics.add.staticGroup();
 
-    let ledge = platforms.create(400, 450, 'ground')
-    ledge.body.immovable = true
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    // platforms.create(300, 300, 'myGround');
+    platforms.create(400, 450, 'ground');
+    platforms.create(100, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 
-    ledge = platforms.create(-75, 350, 'ground')
-    ledge.body.immovable = true
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 100,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 200
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 100,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
+
+
+    
+
 }
 
 
-function update() { }
+function update() { 
+    cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
+}
