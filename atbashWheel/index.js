3fc7c291694eda7d265cdd3abc67b8f1c1c0e952
Shannon Miller <shannon.wlmiller@yahoo.com>
2022-09-18 09:28:53 -0600

    atbash wheel coder complete

--- atbashWheel/index.js
+++ atbashWheel/index.js
@@ -1,6 +1,6 @@
 function atbashWheel(text) {
-    return text.replace('a', 's').replace('b','t')
+    return text.replace('a', 'H').replace('b', 'I').replace('c', 'J').replace('d', 'K').replace('e', 'L').replace('f', 'M').replace('g', 'N').replace('h', 'O').replace('i', 'P').replace('j', 'Q').replace('k', 'R').replace('l', 'S').replace('m', 'T').replace('n', 'U').replace('o', 'V').replace('p', 'W').replace('q', 'X').replace('r', 'Y').replace('s', 'Z').replace('t', 'A').replace('u', 'B').replace('v', 'C').replace('w', 'D').replace('x', 'E').replace('y', 'F').replace('z', 'G')
 }
+//lowercase is the word uppercase is the codeword
 
-
-console.log(atbashWheel('ab'))
+console.log(atbashWheel('talk'))
