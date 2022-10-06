f6b6206b1e897c2984994cffebce2b26158ff7cd
Shannon Miller <shannon.wlmiller@yahoo.com>
2022-10-06 16:32:24 -0600

    added a morse code coder

--- puzzleCoders/index.js
+++ puzzleCoders/index.js
@@ -14,9 +14,14 @@ function text(text) {
     return text.replaceAll(' ', '       ').replaceAll('a', '2  ').replaceAll('b', '2-2  ').replaceAll('c', '2-2-2  ').replaceAll('d', '3  ').replaceAll('e', '3-3  ').replaceAll('f', '3-3-3  ').replaceAll('g', '4  ').replaceAll('h', '4-4  ').replaceAll('i', '4-4-4  ').replaceAll('j', '5  ').replaceAll('k', '5-5  ').replaceAll('l', '5-5-5  ').replaceAll('m', '6  ').replaceAll('n', '6-6  ').replaceAll('o', '6-6-6  ').replaceAll('p', '7  ').replaceAll('q', '7-7  ').replaceAll('r', '7-7-7  ').replaceAll('s', '7-7-7-7  ').replaceAll('t', '8  ').replaceAll('u', '8-8  ').replaceAll('v', '8-8-8  ').replaceAll('w', '9  ').replaceAll('x', '9-9  ').replaceAll('y', '9-9-9  ').replaceAll('z', '9-9-9-9  ')
 } 
 
+function morseCode(text) {
+    return text.replaceAll(' ', '       ').replaceAll('a', '*-  ').replaceAll('b', '-***  ').replaceAll('c', '-*-*  ').replaceAll('d', '-**  ').replaceAll('e', '*  ').replaceAll('f', '**-*  ').replaceAll('g', '--*  ').replaceAll('h', '****  ').replaceAll('i', '**  ').replaceAll('j', '*---  ').replaceAll('k', '-*-  ').replaceAll('l', '*-**  ').replaceAll('m', '--  ').replaceAll('n', '-*  ').replaceAll('o', '---  ').replaceAll('p', '*--*  ').replaceAll('q', '--*-  ').replaceAll('r', '*-*  ').replaceAll('s', '***  ').replaceAll('t', '-  ').replaceAll('u', '**-  ').replaceAll('v', '***-  ').replaceAll('w', '*--  ').replaceAll('x', '-**-  ').replaceAll('y', '-*--  ').replaceAll('z', '--**  ')
+} 
+
 //need to do a morse code one
 
 
-// console.log(atbashWheel('trailer park'))
-console.log(qwerty('behind the fridge'))
-// console.log(text('claws'))
+// console.log(atbashWheel('inside the garden'))
+// console.log(qwerty('behind the couch'))
+// console.log(text('under the kitchen table'))
+console.log(morseCode('hello how are you'))
